8b37b22d9ba0a76a6c36a950febd060bdc077d32
gauravnetfusionlabsllp <gaurav@netfusionlabsllp.com>
2025-11-05 13:36:38 +0530

    made changes on the cheezee pay psp

--- client_rest_api/apps/payment/views.py
+++ client_rest_api/apps/payment/views.py
@@ -64,13 +64,48 @@ class JenaPayPayIn(APIView):
 
             data = request.data.get('data')
             amount = data.get('amount')
-            userToken = data.get('userToken')
+            authToken = data.get('Auth-Token')
             brokesrUserId = data.get('brokesrUserId')
 
-            
+            cursor = connection.cursor(dictionary=True)
+
+            query = """
+                SELECT 
+                    u.full_name, 
+                    u.email, 
+                    u.id AS user_id
+                FROM crmdb.auth_tokens AS t
+                JOIN crmdb.users AS u 
+                    ON u.id = t.user_id
+                WHERE 
+                    t.auth_token = %s
+                    AND t.user_id IS NOT NULL
+            """
+
+            params = (str(authToken),)
+            cursor.execute(query, params)
+            userData = cursor.fetchone()
+
+            if not userData:
+                response['status'] = 'error'
+                response['errorcode'] = status.HTTP_401_UNAUTHORIZED
+                response['reason'] = "User Deatils Not Found!"
+                response['httpstatus'] = status.HTTP_401_UNAUTHORIZED
+                return Response(response, status=response.get('httpstatus'))
+
+            ordRec = OrderDetails.objects.create(
+                userId = str(userData.get('user_id')),
+                full_name = str(userData.get('full_name')),
+                email = str(userData.get('email')),
+                brokerUserId = str(brokesrUserId),
+                amount = amount
+            )
+
+            # if userIdData:
+
             amount = str(amount) + '.00'
             order = {
-                "number": str(uuid.uuid4()).replace('-',''),
+                "number": str(ordRec.orderId).replace('-',''),
                 "amount": str(amount),
                 "currency": "USD",
                 "description": "Amount for the Trading"
@@ -90,8 +125,8 @@ class JenaPayPayIn(APIView):
                 "error_url": JENA_PAY_ERROR_URL,
                 "url_target": "_blank",
                 "customer":{
-                    "name": str('Testing'),
-                    "email": str('test1@gmail.com')
+                    "name": ordRec.full_name,
+                    "email": ordRec.email
                 },
                 "req_token": False,
                 "hash": signature,
@@ -105,7 +140,6 @@ class JenaPayPayIn(APIView):
             
             url = JENA_PAY_PAYIN_URL
             resp = requests.post(url, headers=headers, json=payload)
-            print(resp.text, "-------------------150")
             
             if resp.status_code != 200:
                 response['status'] = "error"
@@ -118,30 +152,31 @@ class JenaPayPayIn(APIView):
             if "redirect_url" in data:
                 data["cashierLink"] = data.pop("redirect_url")
 
-            header = {
-                "Content-Type": "application/json",
-                "x-crm-api-token": "c6420f81-d146-44c1-807c-2462f9210361"
-            }
+            # header = {
+            #     "Content-Type": "application/json",
+            #     "x-crm-api-token": "c6420f81-d146-44c1-807c-2462f9210361"
+            # }
 
-            payload = {
-                "brokerUserId": brokesrUserId,
-                "amount": int(float(amount)),
-                "method": "Crypto",
-                "comment": "Deposit for Trading Account",
-                "commentForUser": "Deposit for Trading Account",
-                "pspId": 1,
-                "pspTransactionId": order.get('number'),
-                "status": "Pending",
-                "normalizedAmount": int(float(amount)),
-                "decisionTime": 0,
-                "declineReason": "string",
-                "brandExternalId": order.get('number')
-            }
+            # payload = {
+            #     "brokerUserId": brokesrUserId,
+            #     "amount": int(float(amount)),
+            #     "method": "Crypto",
+            #     "comment": "Deposit for Trading Account",
+            #     "commentForUser": "Deposit for Trading Account",
+            #     "pspId": 1,
+            #     "pspTransactionId": order.get('number'),
+            #     "status": "Pending",
+            #     "normalizedAmount": int(float(amount)),
+            #     "decisionTime": 0,
+            #     "declineReason": "string",
+            #     "brandExternalId": order.get('number')
+            # }
 
-            crmRes = requests.post("https://apicrm.sgfx.com/SignalsCRM//crm-api/brokers/bankings/deposit/manual", json=payload, headers=header).json()
+            # crmRes = requests.post("https://apicrm.sgfx.com/SignalsCRM//crm-api/brokers/bankings/deposit/manual", json=payload, headers=header).json()
 
-            if crmRes['result']['success']:
-                response['result'] = {"data":data, "crmRes": crmRes}
+            # if crmRes['result']['success']:
+            ordRec.save()
+            response['result'] = {"data":data, "crmRes": "None"}
 
             return Response(response, status=status.HTTP_200_OK)
 
@@ -169,40 +204,45 @@ class JenaPayPayInCallBack(APIView):
  
             try:
                 with transaction.atomic():
-                    cursor = connection.cursor(dictionary=True)
+                    orderId = str(uuid.UUID(order_number))
+                    orderData = OrderDetails.objects.get(orderId = orderId)
+                    print(orderData, "---------------")
 
-                    query = """
-                        SELECT bb.* 
-                        FROM crmdb.broker_banking AS bb 
-                        WHERE bb.psp_transaction_id = %s
-                    """
+                    if orderData.status != "SUCCESS":
+                        cursor = connection.cursor(dictionary=True)
 
-                    params = (str(order_number))
+                        query = """
+                            SELECT bb.* 
+                            FROM crmdb.broker_banking AS bb 
+                            WHERE bb.psp_transaction_id = %s and bb.broker_user_id = %s and bb.user_id = %s
+                        """
 
-                    cursor.execute(query, params)
-                    row = cursor.fetchone()
+                        params = (str(order_number), orderData.brokerUserId, orderData.userId)
 
-                    if row:
-                        brokerBankingId = row['id']
+                        cursor.execute(query, params)
+                        row = cursor.fetchone()
 
-                    payload = {
-                        "brokerBankingId": brokerBankingId,
-                        "method" : "Crypto",
-                        "comment": "Deposit for Trading Account Approved",
-                        "pspTransactionId" : str(order_number),
-                        "decisionTime" : datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    }
+                        # if row:
+                        #     brokerBankingId = row['id']
 
-                    header = {
-                        "Content-Type": "application/json",
-                        "x-crm-api-token": "c6420f81-d146-44c1-807c-2462f9210361"
-                    }
+                        # payload = {
+                        #     "brokerBankingId": brokerBankingId,
+                        #     "method" : "Crypto",
+                        #     "comment": "Deposit for Trading Account Approved",
+                        #     "pspTransactionId" : str(order_number),
+                        #     "decisionTime" : datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        # }
 
-                    crmRes = requests.post("https://apicrm.sgfx.com/SignalsCRM//crm-api/brokers/bankings/deposit/approve", json=payload, headers=header).json()
+                        # header = {
+                        #     "Content-Type": "application/json",
+                        #     "x-crm-api-token": "c6420f81-d146-44c1-807c-2462f9210361"
+                        # }
+
+                        # crmRes = requests.post("https://apicrm.sgfx.com/SignalsCRM//crm-api/brokers/bankings/deposit/approve", json=payload, headers=header).json()
 
 
-                    if crmRes['result']['success']:
-                        return Response({"code": "200", "msg": "success"}, status=status.HTTP_200_OK)
+                    # if crmRes['result']['success']:
+                    return Response({"code": "200", "msg": "success"}, status=status.HTTP_200_OK)
 
             except Exception as e:
                 return Response(f"Error in Transaction: {str(e)}")
@@ -226,22 +266,57 @@ class CheezeePayUPIPayIN(APIView):
             response = {"status": "success", "errorcode": "", "reason": "", "result": "", "httpstatus": status.HTTP_200_OK}
             data = request.data.get('data')
             amount = data.get('amount')
-            userId = request.headers.get('HTTP_AUTH_TOKEN') or request.headers.get('Auth-Token')
+            authToken = data.get('Auth-Token')
             brokerUserId = data.get('brokerUserId')
-            print(brokerUserId)
+
+            cursor = connection.cursor(dictionary=True)
+
+            query = """
+                SELECT 
+                    u.full_name, 
+                    u.email, 
+                    u.id AS user_id
+                FROM crmdb.auth_tokens AS t
+                JOIN crmdb.users AS u 
+                    ON u.id = t.user_id
+                WHERE 
+                    t.auth_token = %s
+                    AND t.user_id IS NOT NULL
+            """
+
+            params = (str(authToken),)
+            cursor.execute(query, params)
+            userData = cursor.fetchone()
+
+            if not userData:
+                response['status'] = 'error'
+                response['errorcode'] = status.HTTP_401_UNAUTHORIZED
+                response['reason'] = "User Deatils Not Found!"
+                response['httpstatus'] = status.HTTP_401_UNAUTHORIZED
+                return Response(response, status=response.get('httpstatus'))
+            
+            ordRec = OrderDetails.objects.create(
+                userId = str(userData.get('user_id')),
+                full_name = str(userData.get('full_name')),
+                email = str(userData.get('email')),
+                brokerUserId = str(brokerUserId),
+                amount = amount,
+                pspName = "CheezeePay UPI"
+            )
+            
 
             payload = {
                 "appId": os.environ['CHEEZEE_PAY_APP_ID'],
                 "merchantId": os.environ['CHEEZEE_PAY_MERCHANT_ID'],
-                "mchOrderNo": str(uuid.uuid4()).replace("-", ''),
+                "mchOrderNo": str(ordRec.orderId).replace("-", ''),
                 "paymentMode": "P2P",
                 "amount": amount,
-                "name": "brijesh",
+                "name": str(ordRec.full_name),
                 "timestamp": str(int(time.time() * 1000)),
                 "notifyUrl": "https://trader.sgfx.com/sign-in",
                 "returnUrl": CHEEZEE_PAY_RETURN_URL,
                 "language": "en",
-                "email": "test@gmail.com",
+                "email": str(ordRec.email),
                 "phone": "+91986475216"
             }
 
@@ -259,7 +334,6 @@ class CheezeePayUPIPayIN(APIView):
             
             if verify_sign(resp.copy(), PlatformPublicKey):
                 
-                print(payload.get('mchOrderNo'), "-------------------------")
                 header = {
                     "Content-Type": "application/json",
                     "x-crm-api-token": "c6420f81-d146-44c1-807c-2462f9210361"
@@ -276,11 +350,13 @@ class CheezeePayUPIPayIN(APIView):
                     "status": "Pending",
                     "normalizedAmount": int(amount),
                     "decisionTime": 0,
-                    "declineReason": "string",
+                    "declineReason": "Error in Payment Check In.",
                     "brandExternalId": payload.get('mchOrderNo')
                 }
 
                 crmRes = requests.post("https://apicrm.sgfx.com/SignalsCRM//crm-api/brokers/bankings/deposit/manual", json=payload, headers=header).json()
+
+                print(crmRes,"p---------------------------")
 
                 if crmRes['result']['success']:
                     response['result'] = {
@@ -288,7 +364,7 @@ class CheezeePayUPIPayIN(APIView):
                         "crmAPI": crmRes
                     }
 
-                return Response(response, status=response.get('httpstatus'))
+                    return Response(response, status=response.get('httpstatus'))
         
             response['result'] = {
                 "data": resp
--- client_rest_api/client_rest_api/settings.py
+++ client_rest_api/client_rest_api/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,10 +116,22 @@ WSGI_APPLICATION = 'client_rest_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ['DB_ENGINE'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ.get('DB_PORT', '5432'),  # optional with default
     }
 }
 
