a8a48ef0fedec863c99e5ed1a76dda23bc30c27d
gauravnetfusionlabsllp <gaurav@netfusionlabsllp.com>
2025-11-05 19:36:58 +0530

    added helper function for sending message through twilio

---
+++ client_rest_api/apps/users/helpers/twilio_sending_message_helpers.py
@@ -0,0 +1,24 @@
+from twilio.rest import Client
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+def send_text_message():
+    try:
+        message = client.messages(
+            body = "This is Testing Message!!!",
+            from_ = "",
+            to = ""
+        )
+
+        print(message, "--------------------150")
+        return message
+    except Exception as e:
+        print(f"Error in Sending message from the Twilio: {str(e)}")
+        return False
