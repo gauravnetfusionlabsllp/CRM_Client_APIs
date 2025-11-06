7bbf3511dad5d30982993aaba99555e04288c130
gauravnetfusionlabsllp <gaurav@netfusionlabsllp.com>
2025-11-06 18:48:35 +0530

    Added phone verify APIs

--- client_rest_api/apps/users/urls.py
+++ client_rest_api/apps/users/urls.py
@@ -5,5 +5,6 @@ from apps.users import views
 urlpatterns = [
    path('check-email/', views.CheckEmail.as_view(), name="check-email"),
    path('get-wp-verify-link/', views.GenerateWPLink.as_view(), name="get-wp-verify-link"),
-   path('verify-user-phone-number/', views.CheckUserPhoneNumber.as_view(), name="verify-user-phone-number"),
+   path('phone-send-otp/', views.CheckUserPhoneNumber.as_view(), name="verify-user-phone-number"),
+   path('verify-phone-otp/', views.VerifyUserPhoneNumber.as_view(), name="verify-phone-otp"),
 ]
