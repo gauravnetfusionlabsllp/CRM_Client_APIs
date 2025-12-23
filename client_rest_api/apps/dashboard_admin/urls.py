8801d11d63b4ce002ddd0eb6272eaf2d90b3a20f
gauravwarise <gauravwarise04@gmail.com>
2025-12-23 12:32:35 +0530

    saint lucia auto kyc

--- client_rest_api/apps/dashboard_admin/urls.py
+++ client_rest_api/apps/dashboard_admin/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
-from apps.dashboard_admin.views import FinancialTransaction, UpdatePSPRate
+from apps.dashboard_admin.views import FinancialTransaction, UpdatePSPRate, KYCApprove
 
 urlpatterns = [
     path('transactions/', FinancialTransaction.as_view(), name="transactions"),
     path('update-psp-rate/', UpdatePSPRate.as_view(), name="update-psp-rate"),
-    path('get-psp-rate/', UpdatePSPRate.as_view(), name="get-psp-rate")
+    path('get-psp-rate/', UpdatePSPRate.as_view(), name="get-psp-rate"),
+   path('user-kyc/', KYCApprove.as_view(), name="user-kyc"),
+
 ]
